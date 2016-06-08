2d62969198428840cb4afd36a5262b09c5117052
Alexander Elbracht <alexander@elbracht.org>
2016-06-08 14:46:23 +0200

    Add array for selected cards

--- src/elementum/controllers/cardpollController.java
+++ src/elementum/controllers/cardpollController.java
@@ -1,5 +1,6 @@
 package elementum.controllers;
 
+import elementum.models.Card;
 import elementum.models.Cards;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
@@ -13,18 +14,19 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class CardpollController {
     private Stage stage;
     private Cards cards;
-    private int cardsCount;
+    private ArrayList<Card> cardsSelected;
     private int cardsLimit = 3;
 
     public CardpollController(Stage stage) throws Exception {
         this.stage = stage;
         this.cards = new Cards();
+        this.cardsSelected = new ArrayList<>();
 
         Parent root = FXMLLoader.load(getClass().getResource("../views/cardpoll.fxml"));
         Scene scene = new Scene(root);
@@ -42,7 +44,7 @@ public class CardpollController {
 
         // Help label
         Label lblHelp = (Label)scene.lookup("#lblHelp");
-        lblHelp.setText(String.format("Wähle %d Karten", cardsLimit - cardsCount));
+        lblHelp.setText(String.format("Wähle %d Karten", cardsLimit - cardsSelected.size()));
 
         // Cards
         for (Node card : scene.lookup("*").lookupAll(".card")) {
@@ -56,21 +58,22 @@ public class CardpollController {
             card.setOnMouseClicked(t -> {
                 ImageView imageView = (ImageView)t.getSource();
                 ObservableList styleClass = imageView.getStyleClass();
+                int imageId = Integer.parseInt(imageView.getId());
 
                 if (styleClass.contains("card-selected")) {
                     // Remove Card
-                    cardsCount--;
+                    cardsSelected.remove(cards.getCards().get(imageId));
                     styleClass.remove("card-selected");
                     btnContinue.setDisable(true);
                 }
                 else {
                     // Add Card
-                    if (cardsCount < cardsLimit - 1) {
-                        cardsCount++;
+                    if (cardsSelected.size() < cardsLimit - 1) {
+                        cardsSelected.add(cards.getCards().get(imageId));
                         styleClass.add("card-selected");
                     }
-                    else if (cardsCount == cardsLimit - 1) {
-                        cardsCount++;
+                    else if (cardsSelected.size() == cardsLimit - 1) {
+                        cardsSelected.add(cards.getCards().get(imageId));
                         styleClass.add("card-selected");
                         btnContinue.setDisable(false);
                     }
@@ -79,23 +82,22 @@ public class CardpollController {
                     }
                 }
 
-                if (cardsCount == cardsLimit) {
+                if (cardsSelected.size() == cardsLimit) {
                     lblHelp.setText("");
                 }
-                else if (cardsCount == cardsLimit - 1) {
+                else if (cardsSelected.size() == cardsLimit - 1) {
                     lblHelp.setText("Wähle 1 Karte");
                 }
-                else if (cardsCount < cardsLimit) {
-                    lblHelp.setText(String.format("Wähle %d Karten", cardsLimit - cardsCount));
+                else if (cardsSelected.size() < cardsLimit) {
+                    lblHelp.setText(String.format("Wähle %d Karten", cardsLimit - cardsSelected.size()));
                 }
-
             });
         }
     }
 
     private void btnContinueAction(ActionEvent event) {
         try {
-            new GameController(stage);
+            new GameController(stage, cardsSelected);
         }
         catch (Exception ex) {
             // TODO: Catch exception
--- src/elementum/controllers/gameController.java
+++ src/elementum/controllers/gameController.java
@@ -1,15 +1,20 @@
 package elementum.controllers;
 
+import elementum.models.Card;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class GameController {
     private Stage stage;
+    private ArrayList<Card> cards;
 
-    public GameController(Stage stage) throws Exception {
+    public GameController(Stage stage, ArrayList<Card> cards) throws Exception {
         this.stage = stage;
+        this.cards = cards;
 
         Parent root = FXMLLoader.load(getClass().getResource("../views/game.fxml"));
         Scene scene = new Scene(root);
