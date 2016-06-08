d1f644213285803c2bd6aa4386bf857fe3e93d46
Alexander Elbracht <alexander@elbracht.org>
2016-06-08 14:36:41 +0200

    Add class to load fonts

---
+++ src/elementum/controllers/FontLoader.java
@@ -0,0 +1,23 @@
+package elementum.controllers;
+
+import java.awt.*;
+import java.io.InputStream;
+
+public class FontLoader {
+    public enum FontType { BOLD, ITALIC, LIGHT, REGULAR }
+
+    public Font getOpenSans(FontType fontType, float size) {
+        try {
+            String fontName = String.format("../assets/OpenSans-%s.ttf", fontType.toString());
+            InputStream inputStream = getClass().getResourceAsStream(fontName);
+            Font font = Font.createFont(java.awt.Font.TRUETYPE_FONT, inputStream);
+            return font.deriveFont(size);
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            // TODO: Catch exception
+        }
+
+        return null;
+    }
+}
