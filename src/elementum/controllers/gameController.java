2a1878150b3cca9eca8686fc3503cb7c44272ee8
Alexander Elbracht <alexander@elbracht.org>
2016-06-08 16:02:23 +0200

    Update game view

--- src/elementum/controllers/gameController.java
+++ src/elementum/controllers/gameController.java
@@ -1,15 +1,18 @@
 package elementum.controllers;
 
 import elementum.models.Card;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 public class GameController {
     private Stage stage;
+    private Stage dialogStage;
     private ArrayList<Card> cards;
 
     public GameController(Stage stage, ArrayList<Card> cards) throws Exception {
@@ -21,5 +24,51 @@ public class GameController {
         scene.getStylesheets().add(getClass().getResource("../assets/main.css").toExternalForm());
 
         stage.setScene(scene);
+
+        // Help Button
+        Button btnHelp = (Button)scene.lookup("#btnHelp");
+        btnHelp.setOnAction(this::btnHelpAction);
+
+        // Back Button
+        Button btnBack = (Button)scene.lookup("#btnBack");
+        btnBack.setOnAction(this::btnBackAction);
+    }
+
+    private void btnHelpAction(ActionEvent event) {
+        //
+    }
+
+    private void btnBackAction(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../views/dialog.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("../assets/main.css").toExternalForm());
+
+            dialogStage = new Stage();
+            dialogStage.setTitle("Spiel beenden");
+            dialogStage.setResizable(false);
+            dialogStage.setScene(scene);
+            dialogStage.show();
+
+            Button btnYes = (Button)scene.lookup("#btnYes");
+            btnYes.setOnAction(e -> {
+                try {
+                    new MainController(stage);
+                }
+                catch (Exception ex) {
+                    // TODO: Catch exception
+                }
+
+                dialogStage.close();
+            });
+
+            Button btnNo = (Button)scene.lookup("#btnNo");
+            btnNo.setOnAction(e -> {
+                dialogStage.close();
+            });
+        }
+        catch (Exception ex) {
+            // TODO: Catch exception
+        }
     }
 }
