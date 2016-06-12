6516aee8bde2c2948512070de80e06cca17ba01f
Alexander Elbracht <alexander@elbracht.org>
2016-06-12 19:36:03 +0200

    Add comments to logging class

--- src/elementum/controllers/utils/Logging.java
+++ src/elementum/controllers/utils/Logging.java
@@ -4,10 +4,15 @@ import java.util.logging.*;
 
 /**
  * @author Alexander Elbracht
+ *
+ * This class log messages in console and in log file
  */
 public class Logging {
     private Logger logger;
 
+    /**
+     * Constructor
+     */
     public Logging() {
         logger = Logger.getLogger(getClass().getName());
 
@@ -23,10 +28,21 @@ public class Logging {
         }
     }
 
+    /**
+     * Log a message
+     * @param level Log level
+     * @param message Message
+     */
     public void log(Level level, String message) {
         logger.log(level, message);
     }
 
+    /**
+     * Log a exception
+     * @param level Log level
+     * @param message Message
+     * @param ex Exception
+     */
     public void log(Level level, String message, Exception ex) {
         logger.log(level, message, ex);
     }
